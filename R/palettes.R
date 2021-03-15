a29bd9bd59d839ea1bbfd5efe9b63e7b18661233
Martin Westgate <martinjwestgate@gmail.com>
2021-03-15 17:21:24 +1100

    Add ALA colour palette
    No particularly useful code here yet; just the hex codes for ALA colours

---
+++ R/palettes.R
@@ -0,0 +1,18 @@
+# build ggplot2 colour palettes based on ALA standard colours
+# following the workflow outlined here:
+# https://drsimonj.svbtle.com/creating-corporate-colour-palettes-for-ggplot2
+
+# palette
+ala_colors <- c(
+  flamingo = "#E06E53", # primary
+  rust = "#B8573E",
+  grey = "#667073",
+  concrete = "EEECEA", # secondary (monochrome)
+  silver = "#9E9E9F",
+  charcoal = "#222322",
+  honey = "#FFC557", # extended
+  pale_moss = "#B7CD96",
+  seafoam = "#6BDAD5",
+  ocean = "#003A70",
+  lavender = "#A19B2",
+  plum = "#691C32"
