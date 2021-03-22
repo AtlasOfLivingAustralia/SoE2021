146757b20f77f77f65083d92f1d1ef55c92aba91
Martin Westgate <martinjwestgate@gmail.com>
2021-03-22 16:58:44 +1100

    add WONS data
    Note: No integration of WONS with rest of dataset yet

--- R/ALA_data.R
+++ R/ALA_data.R
@@ -7,18 +7,9 @@
 #' @importFrom data.table fread rbindlist
 
 
+# function to get the raw data
+# NOTE: this takes a VERY long time to run
 get_ala_data <- function(){
-
-  # start with states, build data.frame with following columns
-    # threatened status
-    # taxonomic group (from ui list, with 'other' category added)
-    # Australian State
-    # Year group (i.e. 5-year intervals 1970-today)
-    # Number of Records
-    # Number of species
-  # then use group functions to sum counts for un-required subcategories in plotting
-  # repeat for IBRA regions
-
 
   # set config (delete before sharing)
   ala_config(
@@ -64,160 +55,42 @@ get_ala_data <- function(){
 # e.g. when I changed select_filters to call a$full_name rather than
 # "Australian Capital Territory" it loaded data, even though a$full_name == NSW
 
-# process data
-files <- paste0("./cache/",
-  c("ACT", "NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"),
-  ".rds")
 
-crosstab_ala_data <- function(files){
-
-  # import
-  data_in <- do.call(rbind, lapply(files, function(a){readRDS(a)}))
-  # subset to rows with usable data
-  # data_in <- data_in[!is.na(data_in$year), ]
-  # data_in <- data_in[data_in$year > 1980, ] # can go older if needed
-  # data_in <- data_in[data_in$species_guid != "", ]
-
-  # classify by year
-  year_start <- seq(1981, 2016, 5)
-  data_in$year_group <- NA
-  # lapply(
-  for(a in seq_along(year_start)){
-    # function(a){
-      data_in$year_group[
-        which(
-          data_in$year >= year_start[a] & data_in$year < (year_start[a] + 5)
-        )] <- a
-    }# )
-  # xtabs(~data_in$year_group)
-  data_in$year_group <- factor(
-    data_in$year_group,
-    levels = seq_along(year_start),
-    labels = paste(year_start, year_start + 4, sep = "-"))
-  # test this
-  # xtabs(~ year + year_group, data = data_in)
-
-  # set up which taxonomic groups to keep
-  data_in$taxon <- NA
-  chordates <- data_in$phylum == "Chordata"
-  data_in$taxon[chordates] <- "Other Vertebrates"
-  data_in$taxon[(data_in$kingdom == "Animalia" & !chordates)] <- "Invertebrates"
-  data_in$taxon[which(data_in$class == "Amphibia")] <- "Amphibians"
-  data_in$taxon[which(data_in$class == "Reptilia")] <- "Reptiles"
-  data_in$taxon[which(data_in$class == "Mammalia")] <- "Mammals"
-  data_in$taxon[which(data_in$class == "Aves")] <- "Birds"
-  data_in$taxon[which(data_in$kingdom == "Plantae")] <- "Plants"
-  data_in$taxon[which(data_in$kingdom == "Fungi")] <- "Fungi"
-  data_in$taxon[is.na(data_in$taxon)] <- "Other"
-
-  # get national parks information
-  data_in$national_parks <- factor(
-    as.numeric(data_in$cAPAD2016Terrestrial != "") + 1,
-    levels = c(1, 2),
-    labels = c("Other Land Use", "National Park"))
-
-  # determine which basis of record to keep (i.e. any 'other' group?)
-  # data_in$basisOfRecord[data_in$basisOfRecord == ""] <- "Not Recorded"
-  data_in$basisOfRecord[
-    !(data_in$basisOfRecord %in% c("HumanObservation", "PreservedSpecimen"))] <- "Other"
-  # xtabs(~data_in$basisOfRecord)
-
+# take data from inst/extdata and save as a cleaned verison in /data
+# first for EPBC
+build_epbc_df <- function(){
   # classify by threatened status
-  # NOT ACHIEVED YET
+  threatened_list <- read.csv("./SoE2021/inst/extdata/EPBC_list.csv")
+  threatened_ala <- select_taxa(threatened_list$scientific_name)
+  # threatened_ala[threatened_ala$match_type == "higherMatch", ]
+  threatened_ala <- threatened_ala[!is.na(threatened_ala$species), ]
+  threatened_df <- merge(threatened_list,
+    threatened_ala[, c("search_term", "taxon_concept_id")],
+    by.x = "scientific_name", by.y = "search_term",
+    all = FALSE)
 
-  # determine which columns you want to cross-tabulate
-  crosstab_columns <- c(
-    "year_group",
-    "taxon",
-    "basisOfRecord",
-    # "threatened_status",
-    "australianStatesAndTerritories",
-    "iBRA7Regions", # takes a long time to run
-    "national_parks")
+  # length(unique(threatened_list$taxon_concept_id)) == nrow(threatened_list)
+  threated_list <- split(threatened_df, threatened_df$taxon_concept_id)
+  # threated_list[which(unlist(lapply(threated_list, nrow)) > 1)]
+  threatened_df <- do.call(rbind, lapply(threated_list, function(a){a[1, ]}))
+  save(threatened_df, file = "./SoE2021/data/threatened_df.RData")
+}
 
-  # save progress
-  # saveRDS(
-  #   data_in[, c("species_guid", crosstab_columns)],
-  #   "./cache/alldata.rds")
-  # NOTE: This takes ages due to large file size
-  # data_in <- readRDS("./cache/alldata.rds")
-
-  combination_list <- do.call(c,
-    lapply(
-      seq_len(3), # maximum number of combinations
-      # seq_along(crosstab_columns),
-      function(a){combn(crosstab_columns, a, simplify = FALSE)}))
-
-  # we never need to combine states and IBRA regions - remove pairs with these attributes
-  combination_list <- combination_list[
-    !unlist(lapply(combination_list, function(a){
-      all(
-        c("australianStatesAndTerritories", "iBRA7Regions") %in% a)
-    }))]
-
-  # get a list of unique values of each entry
-  unique_list <- lapply(data_in[, crosstab_columns], function(x){
-    out <- unique(x)
-    out <- out[out != ""]
-    out <- out[!is.na(out)]
-    return(out)
-  })
-
-  # convert into a data.frame showing every unique combination
-  factor_list <- lapply(combination_list, function(a){
-    unique_tr <- unique_list[a]
-    result_df <- as.data.frame(
-      lapply(expand.grid(unique_tr), function(a){as.character(a)}))
-    return(result_df)
-  })
-  # sum(unlist(lapply(factor_list, nrow))) # check how many calculations are needed
-
-  # use lapply to get crosstabs for all combinations of data that we are interested in
-  xtab_list <- lapply(factor_list, function(a){
-
-    # track progress
-    print(paste0("starting run ", paste(colnames(a), collapse = " & ")))
-    print(Sys.time())
-
-    # for every combination of variable levels, calculate the number of
-    # records and species
-    result_list <- lapply(
-      split(a, seq_len(nrow(a))),
-      function(b){
-        logical_tr <- eval(str2expression(
-            paste(
-              paste0(
-                "data_in$",
-                paste(colnames(b),
-                  paste0("'", b[1, ], "'"),
-                  sep = " == ")),
-              collapse = " & ")
-        ))
-        # return two numbers
-        return(c(
-          n_records = length(which(logical_tr)),
-          n_spp = length(which(unique(data_in$species_guid[logical_tr]) != ""))
-        ))
-      })
-
-    result_df <- cbind(a, do.call(rbind, result_list))
-    return(result_df)
-
-  })#, mc.cores = 7) # end lapply
-
-  names(xtab_list) <- unlist(lapply(
-    combination_list,
-    function(a){paste(a, collapse = "::")}))
-
-  # last stage is to export xtab list and a corresponding index data.frame
-  save(xtab_list, file = "./SoE2021/data/xtab_data.RData")
-
+# Then for Weeds of National Significance (WONS)
+build_wons_df <- function(){
+  wons_df <- read.csv(
+    "./SoE2021/inst/extdata/Weeds_of_National_Significance_(WoNS)_as_at_Feb._2013.csv")[,
+    c("guid", "Supplied.Name")]
+  colnames(wons_df) <- c("species_guid", "wons_supplied_name")
+  wons_df$wons_included <- TRUE
+  wons_df <- wons_df[wons_df$species_guid != "", ]
+  save(wons_df, file = "./SoE2021/data/wons_df.RData")
 }
 
 
+# Sub-functions to support corss-tabulation by various categories
 
-
-# how to group data by year - function required below
+# group data by year
 add_year_group <- function(df){
   var_factor <- cut(df$year, breaks = seq(1980, 2020, 5))
   levels(var_factor) <- paste(
@@ -226,13 +99,8 @@ add_year_group <- function(df){
     sep = "-")
   return(var_factor)
 }
-# earlier approach based on string manipulation
-# year_levels <- levels(var_factor)
-# year_levels <- gsub(",", "-", year_levels)
-# year_levels <- gsub("\\(|\\]", "", year_levels)
-# levels(var_factor) <- year_levels
-# overwrite instead
 
+# by taxonomic group (pretty arbitrary categories)
 add_taxon <- function(df){
   var <- rep(1, nrow(df)) # Other life forms
   var[which(df$kingdom == "Fungi")] <- 2 # "Fungi"
@@ -261,34 +129,16 @@ add_taxon <- function(df){
   return(var_factor)
 }
 
-
-#
-build_epbc_df <- function(){
-  # classify by threatened status
-  threatened_list <- read.csv("./SoE2021/inst/extdata/EPBC_list.csv")
-  threatened_ala <- select_taxa(threatened_list$scientific_name)
-  # threatened_ala[threatened_ala$match_type == "higherMatch", ]
-  threatened_ala <- threatened_ala[!is.na(threatened_ala$species), ]
-  threatened_df <- merge(threatened_list,
-    threatened_ala[, c("search_term", "taxon_concept_id")],
-    by.x = "scientific_name", by.y = "search_term",
-    all = FALSE)
-
-  # length(unique(threatened_list$taxon_concept_id)) == nrow(threatened_list)
-  threated_list <- split(threatened_df, threatened_df$taxon_concept_id)
-  # threated_list[which(unlist(lapply(threated_list, nrow)) > 1)]
-  threatened_df <- do.call(rbind, lapply(threated_list, function(a){a[1, ]}))
-  save(threatened_df, file = "./SoE2021/data/threatened_df.RData")
-}
-
-
+# EPBC status
 add_threatened_status <- function(df){
+  df$order <- seq_len(nrow(df))
   data_out <- merge(df,
     threatened_df[, c("taxon_concept_id",  "conservation_status")],
     by.x = "species_guid",
     by.y = "taxon_concept_id",
     all.x = TRUE,
     all.y = FALSE)
+  data_out <- data_out[order(data_out$order), ] # NOTE: this hasn't been checked
   # convert raw data to a numeric variable
   status_vector <- rep(1, nrow(data_out))
   status_vector[data_out$conservation_status == "Conservation dependent"] <- 2
@@ -312,8 +162,21 @@ add_threatened_status <- function(df){
   return(status_factor)
 }
 
+# Invasive status
+add_invasive_spp <- function(df){
+  df$order <- seq_len(nrow(df))
+  data_out <- merge(df,
+    wons_df[, c("species_guid",  "wons_included")],
+    by = "species_guid",
+    all.x = TRUE,
+    all.y = FALSE)
+  data_out$wons_included[is.na(data_out$wons_included)] <- FALSE
+  data_out <- data_out[order(data_out$order), ] # NOTE: this hasn't been checked
+  return(data_out$wons_included)
+}
 
-
+# aggregator function to group all relevant information at once.
+# Called by crosstab_ala_data.table()
 add_required_cols <- function(df, combns){
   if(any(combns == "year_group")){
     df$year_group <- add_year_group(df)
@@ -327,6 +190,10 @@ add_required_cols <- function(df, combns){
   return(df)
 }
 
+
+# files <- paste0("./cache/",
+#   c("ACT", "NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"),
+#   ".rds")
 
 crosstab_ala_data.table <- function(files){
 
@@ -452,52 +319,3 @@ crosstab_ala_data.table <- function(files){
   save(data_list, file = "./SoE2021/data/data_list.RData")
 
 }
-
-
-# working for original data.table code
-# # import
-# data_list <- lapply(all_files, function(a){data.table::fread(file = a)})
-# data_in <- data.table::rbindlist(data_list)
-#
-# # do a cross-tabulation by year
-# year_xtab <- data_in[, .(.N), keyby = .(year)] # works
-# # print(year_xtab, topn = 200)
-# # then cross-tab by factor
-# # note this is needed because original df too large for factor generation
-# year_xtab$year_group <- create_year_factor(year_xtab$year)
-# year_group_xtab <- year_xtab[, .(sum(N)), keyby = year_group]
-# colnames(year_group_xtab)[2] <- "n_records"
-#
-# # ditto species counts
-# year_species <- data_in[, .(.N), by = .(year, species_guid)]
-# # year_species_count <- year_species[N > 0, .(.N), keyby = .(year)]
-# # print(year_species_count, topn = 100)
-#
-# # take a specific example
-# # taxon_tr <- "urn:lsid:biodiversity.org.au:afd.taxon:ffdaa478-99ef-49e3-9b36-ca2ce0fa8f64"
-# # rows <- which(year_species$species_guid == taxon_tr)
-# # length(rows)
-# # sum(year_species$N[rows])
-# # year_species[rows, ]
-#
-# # any(is.na(year_species$species_guid)) # false - no missing values
-# # any(year_species$N < 1) # false - no zeroes
-# year_species$year_group <- create_year_factor(year_species$year)
-# year_species_count <- year_species[
-#   (species_guid != ""),
-#   .(.N),
-#   keyby = .(year_group, species_guid)]
-# # note that this still includes species_guid == ""
-# # then sum on this
-#
-# # rows2 <- which(year_species_count$species_guid == taxon_tr)
-# # year_species_count[rows2,]
-# # sum(year_species_count$N[rows2])
-#
-# # print(year_species_count, topn = 100)
-# # any(year_species_count$N < 1)
-# year_group_spp_xtab <- year_species_count[, .(sum(N)), keyby = year_group]
-# year_group_xtab$n_species <- year_group_spp_xtab$V1
-#
-# # compare results
-# # xtab_list$year_group # very different, but no obvious error in new code
---
+++ R/OLD_crosstab_ALA_data.R
@@ -0,0 +1,148 @@
+# This is a function to crosstabulate ALA data using base R syntax
+# It has been replaced by code in ALA_data based on {data.table},
+# which is substantially faster
+
+crosstab_ala_data <- function(files){
+
+  # import
+  data_in <- do.call(rbind, lapply(files, function(a){readRDS(a)}))
+  # subset to rows with usable data
+  # data_in <- data_in[!is.na(data_in$year), ]
+  # data_in <- data_in[data_in$year > 1980, ] # can go older if needed
+  # data_in <- data_in[data_in$species_guid != "", ]
+
+  # classify by year
+  year_start <- seq(1981, 2016, 5)
+  data_in$year_group <- NA
+  # lapply(
+  for(a in seq_along(year_start)){
+    # function(a){
+      data_in$year_group[
+        which(
+          data_in$year >= year_start[a] & data_in$year < (year_start[a] + 5)
+        )] <- a
+    }# )
+  # xtabs(~data_in$year_group)
+  data_in$year_group <- factor(
+    data_in$year_group,
+    levels = seq_along(year_start),
+    labels = paste(year_start, year_start + 4, sep = "-"))
+  # test this
+  # xtabs(~ year + year_group, data = data_in)
+
+  # set up which taxonomic groups to keep
+  data_in$taxon <- NA
+  chordates <- data_in$phylum == "Chordata"
+  data_in$taxon[chordates] <- "Other Vertebrates"
+  data_in$taxon[(data_in$kingdom == "Animalia" & !chordates)] <- "Invertebrates"
+  data_in$taxon[which(data_in$class == "Amphibia")] <- "Amphibians"
+  data_in$taxon[which(data_in$class == "Reptilia")] <- "Reptiles"
+  data_in$taxon[which(data_in$class == "Mammalia")] <- "Mammals"
+  data_in$taxon[which(data_in$class == "Aves")] <- "Birds"
+  data_in$taxon[which(data_in$kingdom == "Plantae")] <- "Plants"
+  data_in$taxon[which(data_in$kingdom == "Fungi")] <- "Fungi"
+  data_in$taxon[is.na(data_in$taxon)] <- "Other"
+
+  # get national parks information
+  data_in$national_parks <- factor(
+    as.numeric(data_in$cAPAD2016Terrestrial != "") + 1,
+    levels = c(1, 2),
+    labels = c("Other Land Use", "National Park"))
+
+  # determine which basis of record to keep (i.e. any 'other' group?)
+  # data_in$basisOfRecord[data_in$basisOfRecord == ""] <- "Not Recorded"
+  data_in$basisOfRecord[
+    !(data_in$basisOfRecord %in% c("HumanObservation", "PreservedSpecimen"))] <- "Other"
+  # xtabs(~data_in$basisOfRecord)
+
+  # classify by threatened status
+  # NOT ACHIEVED YET
+
+  # determine which columns you want to cross-tabulate
+  crosstab_columns <- c(
+    "year_group",
+    "taxon",
+    "basisOfRecord",
+    # "threatened_status",
+    "australianStatesAndTerritories",
+    "iBRA7Regions", # takes a long time to run
+    "national_parks")
+
+  # save progress
+  # saveRDS(
+  #   data_in[, c("species_guid", crosstab_columns)],
+  #   "./cache/alldata.rds")
+  # NOTE: This takes ages due to large file size
+  # data_in <- readRDS("./cache/alldata.rds")
+
+  combination_list <- do.call(c,
+    lapply(
+      seq_len(3), # maximum number of combinations
+      # seq_along(crosstab_columns),
+      function(a){combn(crosstab_columns, a, simplify = FALSE)}))
+
+  # we never need to combine states and IBRA regions - remove pairs with these attributes
+  combination_list <- combination_list[
+    !unlist(lapply(combination_list, function(a){
+      all(
+        c("australianStatesAndTerritories", "iBRA7Regions") %in% a)
+    }))]
+
+  # get a list of unique values of each entry
+  unique_list <- lapply(data_in[, crosstab_columns], function(x){
+    out <- unique(x)
+    out <- out[out != ""]
+    out <- out[!is.na(out)]
+    return(out)
+  })
+
+  # convert into a data.frame showing every unique combination
+  factor_list <- lapply(combination_list, function(a){
+    unique_tr <- unique_list[a]
+    result_df <- as.data.frame(
+      lapply(expand.grid(unique_tr), function(a){as.character(a)}))
+    return(result_df)
+  })
+  # sum(unlist(lapply(factor_list, nrow))) # check how many calculations are needed
+
+  # use lapply to get crosstabs for all combinations of data that we are interested in
+  xtab_list <- lapply(factor_list, function(a){
+
+    # track progress
+    print(paste0("starting run ", paste(colnames(a), collapse = " & ")))
+    print(Sys.time())
+
+    # for every combination of variable levels, calculate the number of
+    # records and species
+    result_list <- lapply(
+      split(a, seq_len(nrow(a))),
+      function(b){
+        logical_tr <- eval(str2expression(
+            paste(
+              paste0(
+                "data_in$",
+                paste(colnames(b),
+                  paste0("'", b[1, ], "'"),
+                  sep = " == ")),
+              collapse = " & ")
+        ))
+        # return two numbers
+        return(c(
+          n_records = length(which(logical_tr)),
+          n_spp = length(which(unique(data_in$species_guid[logical_tr]) != ""))
+        ))
+      })
+
+    result_df <- cbind(a, do.call(rbind, result_list))
+    return(result_df)
+
+  })#, mc.cores = 7) # end lapply
+
+  names(xtab_list) <- unlist(lapply(
+    combination_list,
+    function(a){paste(a, collapse = "::")}))
+
+  # last stage is to export xtab list and a corresponding index data.frame
+  save(xtab_list, file = "./SoE2021/data/xtab_data.RData")
+
+}
